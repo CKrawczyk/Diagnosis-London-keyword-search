d4a98c6619e4f075a16181f08bb9cc9925af932e
CKrawczyk <coleman.krawczyk@gmail.com>
2018-12-03 12:09:01 +0000

    Update `build_subject_set.py` for real input data

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ This will result in an `index_normalized.json` file being created.
 
 ## Search the text
 1. Create a `csv` file containing one keyword/phrase per line
-2. Edit line 6 of `build_subject_set.py` to point to this file
+2. Edit line 9 of `build_subject_set.py` to be a list of input files
 3. Edit line 4 to reflect the maximum number of subjects it search should produce
 3. Download the `diagnosis-london-subjects.csv` from the Project Builder (so we can get the subject ID for any existing subjects on the project)
 4. Run `python build_subject_set.py`
 
-This will results in a `subject_file_names.csv` file with three columns:
+This will results in one `subject_file_names_<original input file name>.csv` file for each input `csv` each with three columns:
 
 1. `file_name`: The name of the text file
 2. `keyword_score`: The relevance score (between 0 and 1)
--- build_subject_set.py
+++ build_subject_set.py
@@ -1,27 +1,33 @@
 import pandas
+import nltk
 from search_text import search
 
-MAX_NUMBER = 500
-
-keywords = pandas.read_csv('example_keywords.csv', header=None)[0].tolist()
-result = pandas.DataFrame(search(*keywords, max_number=MAX_NUMBER))
-result.columns = ['file_name', 'keyword_score']
+MAX_NUMBER = 2000
 
 subjects = pandas.read_csv('diagnosis-london-subjects.csv')
 subjects.metadata = subjects.metadata.apply(eval)
 
-# all existing subject
-subject_id_lut = {}
-for sdx, subject in subjects.iterrows():
-    if 'ocr' in subject.metadata:
-        file_name = subject.metadata['ocr']
-    elif 'filename' in subject.metadata:
-        file_name = subject.metadata['filename'] + '.txt'
-    subject_id_lut[file_name] = subject.subject_id
+csv_input_files = [
+    'DL_keywords_A',
+    'DL_keywords_B',
+    'DL_keywords_C',
+    'DL_keywords_D',
+]
 
-# list subject_id if available
-result['subject_id'] = [subject_id_lut[file] if file in subject_id_lut else None for file in result.file_name]
-result.to_csv('subject_file_names.csv', index=False)
+for input_file in csv_input_files:
+    keywords = pandas.read_csv('{0}.csv'.format(input_file), header=None)[0].tolist()
+    result = pandas.DataFrame(search(*keywords, max_number=MAX_NUMBER))
+    result.columns = ['file_name', 'keyword_score']
 
-# This is the best I can do for now
-# Once all subjects have been uploaded this script should also create and link a new subject set
+    # all existing subject
+    subject_id_lut = {}
+    for sdx, subject in subjects.iterrows():
+        if 'ocr' in subject.metadata:
+            file_name = subject.metadata['ocr']
+        elif 'filename' in subject.metadata:
+            file_name = subject.metadata['filename'] + '.txt'
+        subject_id_lut[file_name] = subject.subject_id
+
+    # list subject_id if available
+    result['subject_id'] = [subject_id_lut[file] if file in subject_id_lut else None for file in result.file_name]
+    result.to_csv('subject_file_names_{0}.csv'.format(input_file), index=False)
